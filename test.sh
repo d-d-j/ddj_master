cae9ceaf1b5cf3326edc009cdaaad9fd4fdb1ba7
Tomek Janiszewski <janiszt@gmail.com>
2013-10-27 22:28:42 +0100

    Make tests more random

--- test.sh
+++ test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 while :
 do
-	curl -X POST -d "{\"series\":7,\"tag\":2,\"time\":`date -u +%s`,\"value\":0.5}" http://localhost:8081/series/id/4/data --header "Content-Type:application/json"
+	value=$[100 + (RANDOM % 100)]$[1000 + (RANDOM % 1000)]
+	value=$[RANDOM % 15].${value:1:2}${value:4:3}
+	series=$[(RANDOM % 10)]
+	tag=$[(RANDOM % 10)]
+	curl -X POST -d "{\"series\":$series,\"tag\":$tag,\"time\":`date -u +%s`,\"value\":$value}" http://localhost:8081/series/id/4/data --header "Content-Type:application/json"
 done
