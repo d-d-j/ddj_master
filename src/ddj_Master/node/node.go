dabedfded3656cb50ac231419a190d7922c4babd
Karol Dzitkowski <k.dzitkowski@gmail.com>
2013-12-02 21:54:08 +0100

    Node log in functionality

--- src/ddj_Master/node/node.go
+++ src/ddj_Master/node/node.go
@@ -5,6 +5,9 @@ import (
 	log "code.google.com/p/log4go"
 	"ddj_Master/dto"
 	"net"
+	"ddj_Master/common"
+	"encoding/binary"
+	"bytes"
 )
 
 // Defines a Node with Id, many GpuIds, a connection object, and
@@ -12,27 +15,62 @@ import (
 type Node struct {
 	Communication
 	Id       	int32
+	Status		int32
 	GpuIds		[]int32
 	Stats		Info
 	stop		chan bool
 }
 
-func NewNode(id int32, gpuIds []int32, connection net.Conn) *Node {
+func NewNode(id int32, connection net.Conn) *Node {
 	n := new(Node)
+	n.Status = common.NODE_CONNECTED
 	n.Id = id
-	n.GpuIds = gpuIds
 	n.stop = make(chan bool)
 	n.Communication = makeCommunication(connection)
 	return n
 }
 
 func (n *Node) StartWork() {
-	go n.readerRoutine()
-	go n.senderRoutine()
+	err := n.waitForLogin()
+	if err == nil {
+		go n.readerRoutine()
+		go n.senderRoutine()
+	}
+	log.Debug("Node %d READY", n.Id)
 }
 
 func (n *Node) EndWork(){
 	n.stop <- true
+}
+
+func (n *Node) waitForLogin() error  {
+	buffer := make([]byte, 4)
+	n.Communication.read(buffer)
+	buf := bytes.NewBuffer(buffer)
+	var cudaGpuCount int32
+	err := binary.Read(buf, binary.LittleEndian, &cudaGpuCount)
+	if err != nil {
+		log.Error("Node login error for node ", n.Id)
+		n.Status = common.NODE_ERROR
+		return err
+	}
+	log.Debug("Node received login data (CUDA GPU COUNT = ", cudaGpuCount, ") from node ", n.Id)
+
+	buffer = make([]byte, 4*cudaGpuCount)
+	n.Communication.read(buffer)
+	buf = bytes.NewBuffer(buffer)
+	n.GpuIds = make([]int32, cudaGpuCount)
+	for i:=int32(0); i<cudaGpuCount; i++ {
+		err = binary.Read(buf, binary.LittleEndian, &(n.GpuIds[i]))
+		if err != nil {
+			log.Error("Node login error for node ", n.Id)
+			n.Status = common.NODE_ERROR
+			return err
+		}
+	}
+	log.Debug("Node ", n.Id, " is ready with devices ", n.GpuIds)
+	n.Status = common.NODE_READY
+	return err
 }
 
 func (n *Node) readerRoutine() {
