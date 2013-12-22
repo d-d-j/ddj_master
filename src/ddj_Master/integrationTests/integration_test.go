6ee66ff5efc06ba3e808cc1d0572637a43e992a2
kdutkowski <kdutkowski@gmail.com>
2013-12-22 18:48:18 +0100

    fixes in integration tests

--- src/ddj_Master/integrationTests/integration_test.go
+++ src/ddj_Master/integrationTests/integration_test.go
@@ -14,6 +14,8 @@ func Benchmark_Insert_One_Value(b *testing.B) {
 	json_data := "{\"tag\":1,	\"metric\":2,	\"time\":1383501407,	\"value\":0.5}"
 
 	json_data_reader  := strings.NewReader(json_data)
+
+	// TODO: use cfg values instead of hardcoded port
 	response, err := http.Post("http://localhost:8888/data", "application/json", json_data_reader)
 
 	if response.StatusCode != 202 {
@@ -22,12 +24,18 @@ func Benchmark_Insert_One_Value(b *testing.B) {
 	}
 }
 
-func Benchmark_1000_Values(b *testing.B) {
+func Benchmark_Insert_N_Values(b *testing.B) {
 	client := &http.Client{}
 
-	for i := 0; i < 1000; i++ {
+	b.ResetTimer();
+
+	for i := 0; i < b.N; i++ {
+
+		// TODO: cleanup the object declarations before resetTimer to get reliable execution times
 		data := fmt.Sprintf("{\"tag\":%d, \"metric\":%d, \"time\":%d, \"value\":%f}", rand.Int31n(2) + 1, rand.Int31n(4) + 1, i, float32(i)+2.5)
 
+
+		// TODO: use cfg values instead of hardcoded port
 		req, err := http.NewRequest("POST", "http://localhost:8888/data", strings.NewReader(data))
 
 		req.Header.Set("Content-Type", "application/json")
