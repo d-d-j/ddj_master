b13b42fec77bf61fc44aedd7486961f4897f2fa8
kdutkowski <kdutkowski@gmail.com>
2013-12-22 17:41:31 +0100

    added keep-alive header to insert benchmark

--- src/ddj_Master/integrationTests/integration_test.go
+++ src/ddj_Master/integrationTests/integration_test.go
@@ -4,33 +4,41 @@ import (
 	"testing"
 	"net/http"
 	"strings"
+	"fmt"
+	"math/rand"
 )
+
 
 func Benchmark_Insert_One_Value(b *testing.B) {
 
 	json_data := "{\"tag\":1,	\"metric\":2,	\"time\":1383501407,	\"value\":0.5}"
 
 	json_data_reader  := strings.NewReader(json_data)
-
-
 	response, err := http.Post("http://localhost:8888/data", "application/json", json_data_reader)
 
-	b.Log("Status Code:",response.StatusCode,  err)
-
 	if response.StatusCode != 202 {
+		b.Log("Status Code:",response.StatusCode,  err)
 		b.Fail()
 	}
 }
 
-func Benchmark_Insert_One_Value_And_Select(b *testing.B) {
+func Benchmark_1000_Values(b *testing.B) {
+	client := &http.Client{}
 
-	data := "{\"tag\":2,	\"metric\":3,	\"time\":1383501407,	\"value\":0.5}"
+	for i := 0; i < 1000; i++ {
+		data := fmt.Sprintf("{\"tag\":%d, \"metric\":%d, \"time\":%d, \"value\":%f}", rand.Int31n(2) + 1, rand.Int31n(4) + 1, i, float32(i)+2.5)
 
-	json_data := strings.NewReader(data)
+		req, err := http.NewRequest("POST", "http://localhost:8888/data", strings.NewReader(data))
 
-	http.Post("http://localhost:8888/data", "application/json", json_data)
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Connection", "Keep-Alive")
+		response, err := client.Do(req)
 
-//	response, err := http.Get("http://localhost:8888/data/metric/2/tag/3/time/10-20,30-60/aggregation/none")
-
-//	b.Log("Status Code:",response.StatusCode,  err)
+		if response.StatusCode != 202 {
+			b.Log("Status Code:",response.StatusCode,  err)
+			b.Fail()
+		}
+		response.Body.Close()
+	}
 }
+
