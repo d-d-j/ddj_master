d812b49021a25dd8fa41760baedba6b261d78394
Tomek Janiszewski <janiszt@gmail.com>
2013-12-23 20:41:10 +0100

    Fix integrationTests

--- src/ddj_Master/integrationTests/integration_test.go
+++ src/ddj_Master/integrationTests/integration_test.go
@@ -1,49 +1,146 @@
 package integrationTests
 
 import (
+	"ddj_Master/dto"
+	"encoding/json"
 	"fmt"
-	"math/rand"
+	"io/ioutil"
 	"net/http"
 	"strings"
 	"testing"
 )
 
-func Benchmark_Insert_One_Value(b *testing.B) {
+type RestResponse struct {
+	Error  string
+	TaskId int64
+	Data   []dto.Element
+}
 
-	json_data := "{\"tag\":1,	\"metric\":2,	\"time\":1383501407,	\"value\":0.5}"
-
-	json_data_reader := strings.NewReader(json_data)
-
+const (
+	NUMBER_OF_TAGS_PER_METRICS int = 4
+	NUMBER_OF_METRICS          int = 4
+	INSERTED_DATA              int = 10
 	// TODO: use cfg values instead of hardcoded port
-	response, err := http.Post("http://localhost:8888/data", "application/json", json_data_reader)
+	HOST string = "http://localhost:8888/data"
+)
 
-	if response.StatusCode != 202 {
-		b.Log("Status Code:", response.StatusCode, err)
-		b.Fail()
+var (
+	insert_completed bool
+	data             []string
+	expected         []dto.Element
+)
+
+func SetUp(b *testing.B) {
+
+	if insert_completed != true {
+		client := &http.Client{}
+		data = make([]string, INSERTED_DATA)
+		expected = make([]dto.Element, INSERTED_DATA)
+		for i := 0; i < INSERTED_DATA; i++ {
+			e := *dto.NewElement(int32(i%NUMBER_OF_TAGS_PER_METRICS), int32(i%NUMBER_OF_METRICS), int64(i), 1.0)
+			data[i] = fmt.Sprintf("{\"tag\":%d, \"metric\":%d, \"time\":%d, \"value\":%f}", e.Metric, e.Tag, e.Time, e.Value)
+			expected[i] = e
+		}
+
+		b.Log("Inserting ", INSERTED_DATA, " elements")
+		for i := 0; i < INSERTED_DATA; i++ {
+
+			req, err := http.NewRequest("POST", HOST, strings.NewReader(data[i]))
+			if err != nil {
+				b.Log("Error occurred: ", err)
+				b.FailNow()
+			}
+			req.Header.Set("Content-Type", "application/json")
+			req.Header.Set("Connection", "Keep-Alive")
+			response, err := client.Do(req)
+
+			if response.StatusCode != 202 {
+				b.Log("Status Code:", response.StatusCode, err)
+				b.FailNow()
+			}
+			response.Body.Close()
+		}
+		insert_completed = true
 	}
 }
 
-func Benchmark_Insert_N_Values(b *testing.B) {
-	client := &http.Client{}
+//TODO: Add more select test for filters and aggregations
+
+func Benchmark_Select_First_Value(b *testing.B) {
+
+	SetUp(b)
 
 	b.ResetTimer()
 
-	for i := 0; i < b.N; i++ {
+	selectAll := fmt.Sprintf("%s/metric/0/tag/0/time/0-%d/aggregation/none", HOST, 0)
+	req, err := http.Get(selectAll)
+	defer req.Body.Close()
+	if err != nil {
+		b.Log("Error occurred: ", err)
+	}
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		b.Log("Error occurred: ", err)
+		b.FailNow()
+	}
 
-		// TODO: cleanup the object declarations before resetTimer to get reliable execution times
-		data := fmt.Sprintf("{\"tag\":%d, \"metric\":%d, \"time\":%d, \"value\":%f}", rand.Int31n(2)+1, rand.Int31n(4)+1, i, float32(i)+2.5)
+	b.StopTimer()
 
-		// TODO: use cfg values instead of hardcoded port
-		req, err := http.NewRequest("POST", "http://localhost:8888/data", strings.NewReader(data))
+	response := RestResponse{}
 
-		req.Header.Set("Content-Type", "application/json")
-		req.Header.Set("Connection", "Keep-Alive")
-		response, err := client.Do(req)
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		b.Error("Error occurred: ", err)
+		b.FailNow()
+	}
+	Assert(response, b)
+}
 
-		if response.StatusCode != 202 {
-			b.Log("Status Code:", response.StatusCode, err)
-			b.Fail()
+func Benchmark_Select_N_Values_For_All_Tags_And_Metrics(b *testing.B) {
+
+	SetUp(b)
+	dataCount := 0
+	if b.N > INSERTED_DATA {
+		dataCount = INSERTED_DATA
+	} else {
+		dataCount = b.N
+	}
+
+	b.ResetTimer()
+
+	selectAll := fmt.Sprintf("%s/metric/all/tag/all/time/0-%d/aggregation/none", HOST, dataCount)
+	req, err := http.Get(selectAll)
+	defer req.Body.Close()
+	if err != nil {
+		b.Log("Error occurred: ", err)
+	}
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		b.Log("Error occurred: ", err)
+		b.FailNow()
+	}
+
+	b.StopTimer()
+
+	response := RestResponse{}
+
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		b.Error("Error occurred: ", err)
+		b.FailNow()
+	}
+	Assert(response, b)
+}
+
+func Assert(response RestResponse, b *testing.B) {
+	if len(response.Data) > INSERTED_DATA {
+		b.Error("Too many values returned. Expected less or equal than ", INSERTED_DATA, " but got ", len(response.Data))
+		b.Log("Did you forget to restart server?")
+		b.FailNow()
+	}
+	for index, element := range response.Data {
+		if expected[index].String() != element.String() {
+			b.Error("Expected ", expected[index], " but got ", element)
 		}
-		response.Body.Close()
 	}
 }
