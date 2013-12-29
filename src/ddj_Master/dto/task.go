fbe4b2a2205a14cb0b85fbb1da195c98baf350f7
Tomek Janiszewski <janiszt@gmail.com>
2013-12-29 21:09:25 +0100

    Extend Task with AggregationType

--- src/ddj_Master/dto/task.go
+++ src/ddj_Master/dto/task.go
@@ -6,12 +6,13 @@ import (
 )
 
 type Task struct {
-	Id           int64
-	Type         int32
-	Data         Dto
-	DataSize     int32
-	ResponseChan chan *RestResponse // channel for sending response to (REST API) client
-	ResultChan   chan *RestResponse // channel for sending result to worker
+	Id              int64
+	Type            int32
+	AggregationType int32
+	Data            Dto
+	DataSize        int32
+	ResponseChan    chan *RestResponse // channel for sending response to (REST API) client
+	ResultChan      chan *RestResponse // channel for sending result to worker
 }
 
 type GetTaskRequest struct {
@@ -23,10 +24,21 @@ func NewTask(id int64, request RestRequest, resultChan chan *RestResponse) *Task
 	t := new(Task)
 	t.Id = id
 	t.Type = request.Type
+	t.AggregationType = common.CONST_UNINITIALIZED
 	t.Data = request.Data
 	t.DataSize = int32(request.Data.Size())
 	t.ResponseChan = request.Response
 	t.ResultChan = resultChan
+
+	if t.Type == common.TASK_SELECT {
+		if query, ok := t.Data.(*Query); ok {
+			t.AggregationType = query.AggregationType
+		} else {
+			panic("Type mismatch. TaskType select can be used only with Query data")
+		}
+
+	}
+
 	return t
 }
 
