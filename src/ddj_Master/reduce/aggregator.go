0d573073eda209a2cf8751da302312f66570e510
Tomek Janiszewski <janiszt@gmail.com>
2013-12-29 19:03:26 +0100

    Change input type from Dtos to RestResponse

--- src/ddj_Master/reduce/aggregator.go
+++ src/ddj_Master/reduce/aggregator.go
@@ -2,17 +2,29 @@ package reduce
 
 import (
 	"ddj_Master/dto"
+	"sort"
 )
 
 type Aggregator interface {
-	Aggregate([]dto.Dtos) dto.Dtos
+	Aggregate([][]*dto.RestResponse) dto.Dtos
 }
 
 type NonAggregation struct{}
 
-func (this NonAggregation) Aggregate(input []dto.Dtos) dto.Dtos {
-	if len(input) == 1 {
-		return input[0]
+func (this NonAggregation) Aggregate(input [][]*dto.RestResponse) dto.Dtos {
+
+	if input == nil {
+		return nil
 	}
-	return nil
+
+	output := make([]dto.Dto, 0, len(input))
+	for j := 0; j < len(input); j++ {
+		for i := 0; i < len(input[j]); i++ {
+			output = append(output, input[j][i].Data...)
+		}
+	}
+
+	sort.Sort(dto.ByTime(output))
+
+	return output
 }
--- src/ddj_Master/reduce/aggregator_test.go
+++ src/ddj_Master/reduce/aggregator_test.go
@@ -15,7 +15,7 @@ func Test_NonAggregation_Should_Return_Nil_When_Input_Is_Nil(t *testing.T) {
 }
 
 func Test_NonAggregation_Should_Return_Empty_Slice_If_Input_Containse_Only_nil(t *testing.T) {
-	input := make([]dto.Dtos, 5)
+	input := make([][]*dto.RestResponse, 5)
 
 	actual := nonAggr.Aggregate(input)
 	if len(actual) != 0 && actual != nil {
@@ -23,12 +23,25 @@ func Test_NonAggregation_Should_Return_Empty_Slice_If_Input_Containse_Only_nil(t
 	}
 }
 
-func Test_NonAggregation_Should_Return_Same_Slice_But_As_Input_If_There_Was_Only_One_Element_In_Input_Slice(t *testing.T) {
-	input := make([]dto.Dtos, 1)
+func Test_NonAggregation_Should_Return_Same_Slice_As_Input_If_There_Was_Only_One_Element_In_Input_Slice(t *testing.T) {
+	input := make([][]*dto.RestResponse, 1)
+	input[0] = make([]*dto.RestResponse, 1)
 	expected := dto.Dtos{dto.NewElement(1, 2, 0, 0.33)}
-	input[0] = expected
+	input[0][0] = dto.NewRestResponse("", 0, expected)
 
 	actual := nonAggr.Aggregate(input)
+	if actual.String() != expected.String() {
+		t.Error("Expected ", expected, " but got ", actual)
+	}
+}
+
+func Test_NonAggregation_Should_Return_Same_Slice_But_Sorted_As_Input_If_There_Was_Only_One_Slice_In_Input(t *testing.T) {
+	input := make([][]*dto.RestResponse, 1)
+	input[0] = make([]*dto.RestResponse, 1)
+	input[0][0] = dto.NewRestResponse("", 0, dto.Dtos{dto.NewElement(1, 4, 2, 0.33), dto.NewElement(1, 2, 1, 0.33), dto.NewElement(1, 0, 0, 0.33)})
+
+	actual := nonAggr.Aggregate(input)
+	expected := dto.Dtos{dto.NewElement(1, 0, 0, 0.33), dto.NewElement(1, 2, 1, 0.33), dto.NewElement(1, 4, 2, 0.33)}
 	if actual.String() != expected.String() {
 		t.Error("Expected ", expected, " but got ", actual)
 	}
