2d86a8f9ae7189b95e9aeda28ef607b2ea7206bb
Tomek Janiszewski <janiszt@gmail.com>
2013-12-30 17:18:02 +0100

    Add more tests

--- src/ddj_Master/task/job_test.go
+++ src/ddj_Master/task/job_test.go
@@ -29,7 +29,7 @@ func Test_ParseResultsToElements_Should_Return_Empty_Slice_For_Empty_Data_Imput(
 	}
 }
 
-func Test_ParseResultsToElements_Should_Return_One_Elements_When_Called_With_One_In_Slice(t *testing.T) {
+func Test_ParseResultsToElements_Should_Return_One_Element_When_Called_With_One_In_Slice(t *testing.T) {
 	// PREPARE DATA FOR TEST
 	expected := dto.NewElement(1, 2, 3, 0.33)
 	data, err := expected.Encode()
@@ -40,5 +40,56 @@ func Test_ParseResultsToElements_Should_Return_One_Elements_When_Called_With_One
 	actual := parseResultsToElements([]*dto.Result{result})
 	if actual[0].String() != expected.String() {
 		t.Error("Expected empty slice but got ", actual)
+	}
+}
+
+func Test_ParseResultsToElements_Should_Return_All_Elements_From_Single_Input(t *testing.T) {
+	// PREPARE DATA FOR TEST
+	expected := dto.Dtos{dto.NewElement(1, 2, 3, 0.33), dto.NewElement(4, 5, 6, 0.66), dto.NewElement(7, 8, 9, 0.99)}
+	data, err := expected.Encode()
+	if err != nil {
+		t.Error("Error occurred", err)
+	}
+	result := dto.NewResult(0, common.TASK_SELECT, int32(expected.Size()), data)
+	actual := parseResultsToElements([]*dto.Result{result})
+	// ASSERTIONS
+
+	if len(actual) != 3 {
+		t.Error("Wrong data returned. Expected ", len(expected), " values")
+	}
+
+	for index, elem := range actual {
+		AssertEqual(expected[index], elem, t)
+	}
+}
+
+func Test_ParseResultsToElements_Should_Return_All_Elements_From_Input(t *testing.T) {
+	// PREPARE DATA FOR TEST
+	expected := dto.Dtos{
+		dto.NewElement(1, 2, 3, 0.33), dto.NewElement(4, 5, 6, 0.66), dto.NewElement(7, 8, 9, 0.99),
+	}
+	data, err := expected.Encode()
+	if err != nil {
+		t.Error("Error occurred", err)
+	}
+	result := dto.NewResult(0, common.TASK_SELECT, int32(expected.Size()), data)
+	actual := parseResultsToElements([]*dto.Result{result, result, result, result})
+	// ASSERTIONS
+
+	expected = append(expected, expected...)
+	expected = append(expected, expected...)
+
+	if len(actual) != len(expected) {
+		t.Error("Wrong data returned. Expected ", len(expected), " values", " but got ", len(actual))
+	}
+
+	for index, elem := range actual {
+		AssertEqual(expected[index], elem, t)
+	}
+}
+
+func AssertEqual(expected, actual dto.Dto, t *testing.T) {
+	if expected.String() != actual.String() {
+		t.Error("Expected: ", expected, " but got: ", actual)
 	}
 }
