f7503c5c31302fba8cca04204e2619799f0aa301
Tomek Janiszewski <janiszt@gmail.com>
2013-12-31 15:00:06 +0100

    Add timeout in GatherAllResponses

--- src/ddj_Master/task/job.go
+++ src/ddj_Master/task/job.go
@@ -6,6 +6,7 @@ import (
 	"ddj_Master/dto"
 	"ddj_Master/node"
 	"ddj_Master/reduce"
+	"time"
 )
 
 type job func(dto.RestRequest) bool
@@ -215,7 +216,14 @@ func GatherAllResponses(numResponses int, responseChan chan *dto.Result) []*dto.
 
 	// WAIT FOR ALL RESPONSES
 	for i := 0; i < numResponses; i++ {
-		responses[i] = <-responseChan
+		timeout := time.After(1 * time.Second)
+		select {
+		case response := <-responseChan:
+			responses[i] = response
+		case <-timeout:
+			panic("TIMEOUT")
+		}
+
 		log.Finest("Got task result [%d/%d] - %s", i, numResponses, responses[i])
 	}
 
