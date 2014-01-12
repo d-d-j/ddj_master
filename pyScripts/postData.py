986c7dcc1a66a3770240c97c2377781bf2d1ea29
kdutkowski <kdutkowski@gmail.com>
2014-01-12 18:19:58 +0100

    added session to have keep-alive header

--- pyScripts/postData.py
+++ pyScripts/postData.py
@@ -18,21 +18,26 @@ def postValues(number, tag, metric, url, func):
     print "\ttag:", tag
     print "\tmetric:", metric
 
+    s = requests.Session()
+
     headers = {'content-type': 'application/json'}
+    req = requests.Request('POST', url,
+                           headers=headers)
     for i in range(1, number + 1):
         payload = {"tag": tag, "metric": metric, "time": i, "value": func(0.01 * i)}
+        req.data = json.dumps(payload)
+
+        prepped = req.prepare()
 
         try:
-            response = requests.post(url, data=json.dumps(payload), headers=headers)
+            response = s.send(prepped)
             if response.status_code != 202:
                 print "Got response: ", response.status_code
                 sys.exit(1)
 
         except requests.exceptions.RequestException as e:    # This is the correct syntax
-                print e
-                sys.exit(1)
-
-
+            print e
+            sys.exit(1)
 
         if i % 100 == 0:
             print i
@@ -47,7 +52,8 @@ def main():
     parser.add_argument('-c', '--cos', help='use cosine values instead of sine', action='store_true')
     parser.add_argument('-n', type=int, dest='n', default=1000, help='number of records to post (default: 1000)')
     parser.add_argument('-t', '--tag', type=int, dest='tag', default=1, help='tag of data to post (default: 1)')
-    parser.add_argument('-m', '--metric', type=int, dest='metric', default=1, help='metric of data to post (default: 1)')
+    parser.add_argument('-m', '--metric', type=int, dest='metric', default=1,
+                        help='metric of data to post (default: 1)')
 
     args = parser.parse_args()
 
