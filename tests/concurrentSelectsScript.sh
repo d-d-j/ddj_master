37c6eba84876366c398fb0192c4ee1b7dda44135
kdutkowski <kdutkowski@gmail.com>
2014-01-20 00:25:22 +0100

    fixed typos in script

--- tests/concurrentSelectsScript.sh
+++ tests/concurrentSelectsScript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python concurrenSelects.py -s -nv 10000 -ns 1000 -nt 20 > concurrentSelectsResults10000.txt
-python concurrenSelects.py -s -nv 100000 -ns 1000 -nt 20 > concurrentSelectsResults100000.txt
-python concurrenSelects.py -s -nv 1000000 -ns 1000 -nt 20 > concurrentSelectsResults1000000.txt
+python concurrentSelects.py -s -nv 10000 -ns 1000 -mt 20 > concurrentSelectsResults10000.txt
+python concurrentSelects.py -s -nv 100000 -ns 1000 -mt 20 > concurrentSelectsResults100000.txt
+python concurrentSelects.py -s -nv 1000000 -ns 1000 -mt 20 > concurrentSelectsResults1000000.txt
